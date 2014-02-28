dee1e9af108a95ec487467152b9e1be259f89cb9
Peter Gorman <petegorman@outlook.com>
2014-03-01 01:17:22 +1100

    Portfolio WordPress

--- wp-content/themes/blankslate/header-portfolio.php
+++ wp-content/themes/blankslate/header-portfolio.php
@@ -2,21 +2,15 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title(' | ', true, 'right'); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="icon" type="image/x-icon" href="<?php echo site_url(); ?>/wp-content/themes/blankslate/favicon.ico" />
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/blankslate/includes/css/global-portfolio.css">
 
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>/wp-content/themes/blankslate/portfolio/css/global.css">
+<script src="//use.edgefonts.net/raleway;open-sans:n3,i3,n4,i4,n6,i6,n7,i7,n8,i8:all.js"></script>
+<script src="<?php echo site_url(); ?><?php echo site_url(); ?>/wp-content/themes/blankslate/portfolio/Scripts/swfobject_modified.js" type="text/javascript"></script>
+<!--[if lt IE 9]><script src="dist/html5shiv.js"></script><![endif]-->
 </head>
+
 <body <?php body_class(); ?>>
-
-
-<div class="container">
--- wp-content/themes/blankslate/portfolio.php
+++ wp-content/themes/blankslate/portfolio.php
@@ -6,52 +6,68 @@ Template Name: Portfolio
 
 <?php get_header('portfolio'); ?>
 
-	<header class="row">
-		<div class="col-md-12">
-		<h1>EDMs</h1>
-		</div>
-	</header>
-	
-	<section class="row">
-	
-		<div class="col-md-12">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+<header>
+	<h1>Td.</h1>
+</header>
+
+<section>
+	<div class="row">
+		<h2>Display Ads</h2>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 			the_content();
 			endwhile; else: ?>
 			<p>Sorry, no posts matched your criteria.</p>
-			<?php endif; ?>
+		<?php endif; ?>
+	</div>
+	<?php if( get_field('banner_sample') ): ?>
+	<div class="row">
+		<?php while( has_sub_field('banner_sample') ): ?>
+		<div class="holder">
+			<h3><?php the_sub_field('banner_content'); ?></h3>
+			<div class="work-banner">
 			
-			<?php if( get_field('banner_sample') ): ?>
-			
-			<div class="row">
-			<?php while( has_sub_field('banner_sample') ): ?>
+				<!-- Start Flash -->
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="<?php the_sub_field('banner_width'); ?>" height="<?php the_sub_field('banner_height'); ?>" id="FlashID" title="<?php the_sub_field('banner_content'); ?>">
+					<param name="movie" value="<?php the_sub_field('add_a_banner'); ?>">
+					<param name="quality" value="high">
+					<param name="wmode" value="opaque">
+					<param name="swfversion" value="9.0.115.0">
+					<!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+					<param name="expressinstall" value="Scripts/expressInstall.swf">
+					<!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+					<!--[if !IE]>-->
+					<object type="application/x-shockwave-flash" data="<?php the_sub_field('add_a_banner'); ?>" width="<?php the_sub_field('banner_width'); ?>" height="<?php the_sub_field('banner_height'); ?>">
+						<!--<![endif]-->
+						<param name="quality" value="high">
+						<param name="wmode" value="opaque">
+						<param name="swfversion" value="9.0.115.0">
+						<param name="expressinstall" value="Scripts/expressInstall.swf">
+						<!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+						<div>
+							<h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+							<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+						</div>
+						<!--[if !IE]>-->
+					</object>
+					<!--<![endif]-->
+				</object>
+				<!-- END Flash -->
 				
-				<div class="col-md-4">
-					<img src="<?php the_sub_field('add_a_banner'); ?>" alt="<?php the_sub_field('alt'); ?>" width="100%" />
-					<p><?php the_sub_field('banner_content'); ?></p>
-				</div>
-				
-			<?php endwhile; ?>
 			</div>
-			
-			<?php endif; ?>
-			
-			<?php if( get_field('EDM_sample') ): ?>
-			
-			<div class="row">
-			<?php while( has_sub_field('EDM_sample') ): ?>
-				
-				<div class="col-md-6 work-sample">
-					<img src="<?php the_sub_field('add_an_edm'); ?>" alt="<?php the_sub_field('edm_content'); ?>" />
-					<p><?php the_sub_field('edm_content'); ?></p>
-				</div>
-				
-			<?php endwhile; ?>
-			</div>
-			
-			<?php endif; ?>
+			<!-- END Banner -->
 			
 		</div>
-	
-	</section>
-<?php get_footer(); ?>
+		<!-- END Holder -->
+		<?php endwhile; ?>
+		
+	</div>
+	<!-- END Row -->
+	<?php endif; ?>
+</section>
+<script type="text/javascript">
+swfobject.registerObject("FlashID");
+</script>
+</body>
+</html>
+
+<!--<?php get_footer(); ?>-->
