abda90efca3a2bf44638db970117969ff1129564
Peter Gorman <pete@thisdigital.com.au>
2014-03-28 11:01:41 +1100

    SEO

--- wp-config_LIVE.php
+++ wp-config_LIVE.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'thisdigital');
+define('DB_NAME', 'pixelabc_thisdigital');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'pixelabc_pete');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Elle@h01');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
--- wp-content/themes/blankslate/page.php
+++ wp-content/themes/blankslate/page.php
@@ -3,7 +3,21 @@
 		<div class="col-md-6 col-md-offset-3 td-logo">
 			<img src="<?php echo site_url(); ?>/HTML_Build/img/td_logo_01.gif" alt="This Digital" title="This Digital"/>
 			
-			<h1>Specialists in Banner and EDM design and development</h1>
+			<h1>This Digital is a creative agency based in Sydney specialising in digital design and devleopment for small and medium sized businesses.</h1>
+			
+			<p>We offer services in:</p>
+			
+			<ul>
+			
+				<li>Company website design and development</li>
+				<li>Landing page, microsite and startup solutions</li>
+				<li>Digital marketing - EDM and Banner ad design and devleopment</li>
+				<li>Social media campaigns</li>
+			
+			</ul>
+			
+			
+			
 		</div>
 	</header>
 	
@@ -15,15 +29,7 @@
 			endwhile; else: ?>
 			<p>Sorry, no posts matched your criteria.</p>
 			<?php endif; ?>
-			<!--<form>
-				<label for="name">Name</label><br>
-				<input type="text" id="name" name="Name" placeholder="What's your name?" /><br>
-				<label for="email">Email</label><br>
-				<input type="text" id="email" name="Email" placeholder="How should I reply?" /><br>
-				<label for="message">Message</label><br>
-				<textarea name="message" rows="2" id="message" placeholder="Fill me in..." type="text" size="2"></textarea><br>
-				<input type="submit" id="submit" name="submit" value="Go" class="btn btn-info btn-lrg td_btn" />
-			</form>-->
+
 		</div>
 	
 	</section>
